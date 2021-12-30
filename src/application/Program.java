ae601710f031d40533cb7e88f7b86eb62057d1ed
João Paulo <45145741+joao5142@users.noreply.github.com>
2021-12-29 21:58:40 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,16 @@ import java.util.Set;
 import entities.LogEntry;
 
 public class Program {
+	/*
+	
+		Um site de internet registra um log de acessos dos usuários. Um
+		registro de log consiste no nome de usuário (apenas uma palavra) e o
+		instante em que o usuário acessou o site no padrão ISO 8601,
+		separados por espaço, conforme exemplo. Fazer um programa que leia
+		o log de acessos a partir de um arquivo, e daí informe quantos usuários
+		distintos acessaram o site.
+	
+	*/
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Locale.setDefault(Locale.US);
